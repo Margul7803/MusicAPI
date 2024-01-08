de6c896145805e13d8d35ec2884cabd01e983746
Mario ABOU JAMRA <83760967+Margul7803@users.noreply.github.com>
2024-01-08 17:12:59 +0100

    Controller (#18)
    * UserController
    
    init user entity and controller for loginsignup
    
    * Update LoginSignUpController.php
    
    * first commit
    
    * temp
    
    * salut
    
    * Controller
    
    * Update all Controller and update base.html.twig, file connexion
    
    * add work from fork
    
    * ta testdsgsdgdsg
    
    * ttttt
    
    ---------
    
    Co-authored-by: nathan <148321446+datawolfdev@users.noreply.github.com>
    Co-authored-by: Thomas <123959293+thomas-stecinski@users.noreply.github.com>
    Co-authored-by: Mario <mario.aboujamra@weblib.eu>

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,12 +5,14 @@ namespace App\Entity;
 use ApiPlatform\Metadata\ApiResource;
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
 #[ApiResource]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -30,20 +32,23 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private ?string $password = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 255, nullable: true)]
     private ?string $pseudo = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 255, nullable: true)]
     private ?string $prenom = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 255, nullable: true)]
     private ?string $nom = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 255, nullable: true)]
     private ?string $tel = null;
 
     #[ORM\OneToOne(inversedBy: 'utilisateur', cascade: ['persist', 'remove'])]
     private ?Artiste $Artiste = null;
+
+    #[ORM\Column(type: 'boolean')]
+    private $isVerified = false;
 
     public function getId(): ?int
     {
@@ -171,6 +176,18 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setArtiste(?Artiste $Artiste): static
     {
         $this->Artiste = $Artiste;
+
+        return $this;
+    }
+
+    public function isVerified(): bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setIsVerified(bool $isVerified): static
+    {
+        $this->isVerified = $isVerified;
 
         return $this;
     }
