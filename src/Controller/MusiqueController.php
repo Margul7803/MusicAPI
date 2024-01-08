81572c1de4c5d3115789d94910b14d8d278cd60b
tcheio <thomas.cheio0105@gmail.com>
2024-01-08 14:22:03 +0100

    Liste music

---
+++ src/Controller/MusiqueController.php
@@ -0,0 +1,58 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Musique;
+
+class MusiqueController extends AbstractController
+{
+    #[Route('/musiques', name: 'list_musiques', methods: ['GET'])]
+    public function listMusiques(ManagerRegistry $orm): JsonResponse
+    {
+        $musiques = $orm->getRepository(Musique::class)->findAll();
+        $musiquesArray = [];
+        foreach ($musiques as $musique) {
+            $musiquesArray[] = [
+                'id' => $musique->getId(),
+                'titre' => $musique->getTitre(),
+                'duree' => $musique->getDuree(),
+                'dateSortie' => $musique->getDateSortie()->format('Y-m-d'),
+                'parole' => $musique->getParole(),
+            ];
+        }
+        return $this->json($musiquesArray);
+    }
+
+    #[Route('/musiques/{id}', name: 'list_musiques_albums', methods: ['GET'])]
+    public function listMusiquesAlb(ManagerRegistry $orm, $id): Response
+    {
+        $musiques = $orm->getRepository(Musique::class)->findAll();
+        $musiquesArray = [];
+        foreach ($musiques as $musique) {
+            $musiquesArray[] = [
+                'id' => $musique->getId(),
+                'titre' => $musique->getTitre(),
+                'duree' => $musique->getDuree(),
+                'dateSortie' => $musique->getDateSortie()->format('Y-m-d'),
+                'parole' => $musique->getParole(),
+            ];
+        }
+        $musiqueAlb = [];
+        foreach($musiquesArray as $musique) {
+            if ($musique->getAlbum() == $id){
+            $musiqueAlb[] = [
+                'id' => $musique->getId(),
+                'titre' => $musique->getTitre(),
+                'duree' => $musique->getDuree(),
+                'dateSortie' => $musique->getDateSortie()->format('Y-m-d'),
+                'parole' => $musique->getParole(),
+            ];
+        }
+        }
+        return $this->json($musiqueAlb);
+    }
+}
+
