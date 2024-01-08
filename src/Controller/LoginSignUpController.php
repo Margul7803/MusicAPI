3c5793dd338c5fca65ba0bb922ee5724b0eedf03
nathan <148321446+datawolfdev@users.noreply.github.com>
2024-01-08 11:15:38 +0100

    Ta mere

---
+++ src/Controller/LoginSignUpController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LoginSignUpController extends AbstractController
+{
+    /**
+     * @Route("/login", name="login_form")
+     */
+    public function login(): Response
+    {
+        return $this->render('LoginSignUp/login.html.twig', [
+            'login' => 'ta mere',
+        ]);
+    }
+
+    /**
+     * @Route("/signup", name="signup_form")
+     */
+    public function signup(): Response
+    {
+        return $this->render('LoginSignUp/signup.html.twig', [
+            'signup' => 'ta mere',
+        ]);
+    }
+}
