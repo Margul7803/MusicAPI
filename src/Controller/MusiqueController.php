98f78ccee5abcf8962daba8fc19306dc23507a58
tcheio <thomas.cheio0105@gmail.com>
2024-01-09 13:57:57 +0100

    MUSIQUE PAR ALBUMS

--- src/Controller/MusiqueController.php
+++ src/Controller/MusiqueController.php
@@ -2,7 +2,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Musique;
@@ -11,7 +11,7 @@ use App\Entity\Musique;
 class MusiqueController extends AbstractController
 {
     #[Route('/musiques', name: 'list_musiques', methods: ['GET'])]
-    public function listMusiques(ManagerRegistry $orm): JsonResponse
+    public function listMusiques(ManagerRegistry $orm): Response
     {
         $musiques = $orm->getRepository(Musique::class)->findAll();
         $musiquesArray = [];
@@ -24,28 +24,32 @@ class MusiqueController extends AbstractController
                 'parole' => $musique->getParole(),
             ];
         }
-        return $this->json($musiquesArray);
+
+        // Utilisez la classe Response pour retourner une réponse classique
+        return new Response(json_encode($musiquesArray), 200, ['Content-Type' => 'application/json']);
     }
 
     #[Route('/musiques/{id}', name: 'list_musiques_albums', methods: ['GET'])]
-    public function listMusiquesAlb(ManagerRegistry $orm, $id): JsonResponse
+    public function listMusiquesAlb(ManagerRegistry $orm, $id): Response
     {
         $musiques = $orm->getRepository(Musique::class)->findAll();
         $musiquesArray = [];
         foreach ($musiques as $musique) {
             $alb = $musique->getAlbum();
-            if ($alb->getId() == $id){
-            $musiquesArray[] = [
-                'id' => $musique->getId(),
-                'titre' => $musique->getTitre(),
-                'duree' => $musique->getDuree(),
-                'dateSortie' => $musique->getDateSortie()->format('Y-m-d'),
-                'parole' => $musique->getParole(),
-            ];
+            if ($alb->getId() == $id) {
+                $musiquesArray[] = [
+                    'id' => $musique->getId(),
+                    'titre' => $musique->getTitre(),
+                    'duree' => $musique->getDuree(),
+                    'dateSortie' => $musique->getDateSortie()->format('Y-m-d'),
+                    'parole' => $musique->getParole(),
+                ];
+            }
         }
-        }
-        
-        return $this->json($musiquesArray);
+
+        // Utilisez la classe Response pour retourner une réponse classique
+        return $this->render('musique/index.html.twig',
+                            ['liste' => $musiquesArray]);
     }
 }
 
