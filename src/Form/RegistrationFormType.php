e5af9cea13dcd2677910bd953d39a791382251c0
Mario ABOU JAMRA <83760967+Margul7803@users.noreply.github.com>
2024-01-10 12:18:12 +0100

    Controller (#20)
    * UserController
    
    init user entity and controller for loginsignup
    
    * Update LoginSignUpController.php
    
    * first commit
    
    * temp
    
    * salut
    
    * Controller
    
    * Update all Controller and update base.html.twig, file connexion
    
    * add work from fork
    
    * ta testdsgsdgdsg
    
    * ttttt
    
    * ta mere mario
    
    * test
    
    * Update base.html.twig
    
    * ta mere
    
    * Update base.html.twig
    
    ---------
    
    Co-authored-by: nathan <148321446+datawolfdev@users.noreply.github.com>
    Co-authored-by: Thomas <123959293+thomas-stecinski@users.noreply.github.com>
    Co-authored-by: Mario <mario.aboujamra@weblib.eu>

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -11,6 +11,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class RegistrationFormType extends AbstractType
 {
@@ -26,9 +27,31 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
+            ->add('prenom', TextType::class, [
+                'required' => false,
+                'constraints' => [
+                    new Length(['max' => 255])
+                ]
+            ])
+            ->add('nom', TextType::class, [
+                'required' => false,
+                'constraints' => [
+                    new Length(['max' => 255])
+                ]
+            ])
+            ->add('pseudo', TextType::class, [
+                'required' => false,
+                'constraints' => [
+                    new Length(['max' => 255])
+                ]
+            ])
+            ->add('tel', TextType::class, [
+                'required' => false,
+                'constraints' => [
+                    new Length(['max' => 20])
+                ]
+            ])
+            ->add('password', PasswordType::class, [
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
@@ -38,7 +61,20 @@ class RegistrationFormType extends AbstractType
                     new Length([
                         'min' => 6,
                         'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
+            ])
+            ->add('confirm_Password', PasswordType::class, [
+                'mapped' => false,
+                'attr' => ['autocomplete' => 'new-password'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
                         'max' => 4096,
                     ]),
                 ],
